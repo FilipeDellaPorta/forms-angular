fde6181e6932799ef76f1c3d3e790eed5bf65a37
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-03-20 13:28:57 -0300

    feat: :sparkles: add: método que faz a busca do cep a partir do input no template

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ConsultaCepService } from '../service/consulta-cep.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,9 +9,21 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private consultaCepService: ConsultaCepService
+  ) {}
 
   ngOnInit(): void {}
+
+  consultaCEP(event: any) {
+    const cep = event.target.value;
+    return this.consultaCepService
+      .getConsultaCep(cep)
+      .subscribe((resultado: any) => {
+        console.log(resultado);
+      });
+  }
 
   cadastrar(form: NgForm) {
     if (form.valid) {
